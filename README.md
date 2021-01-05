82756e2dc82e65f922074d44930b6b4c6065f419
Full Stack Developer <leeshimin104@outlook.com>
2021-01-05 19:52:47 +0800

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Travel-Website-Spring-Boot
+# Yatra
+
+ This website is built by Spring Boot
+
+*Live (https://www.yatra.com/)
+
+
+**Browser support:**
+
++ **Desktop:** IE11+, Chrome, Firefox, Safari
++ **Mobile:** Android browser (on Android 4.4+), Safari (on iOS 7+), Google Chrome, Opera mini
+
+Other browsers (like Opera on Blink engine) are also supported, but not tested.
---
+++ site/src/main/java/sagan/site/guides/TutorialController.java
@@ -0,0 +1,48 @@
+package sagan.site.guides;
+
+import sagan.site.support.nav.Navigation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sagan.site.support.nav.Section;
+
+/**
+ * Controller that handles requests for tutorial docs at /guides/tutorials.
+ *
+ * @see GettingStartedGuideController
+ * @see TopicalController
+ */
+@Controller
+@Navigation(Section.GUIDES)
+@RequestMapping("/guides/tutorials")
+class TutorialController {
+
+    private Tutorials tutorials;
+
+    @Autowired
+    public TutorialController(Tutorials tutorials) {
+        this.tutorials = tutorials;
+    }
+
+    @RequestMapping("/{tutorial}")
+    public String viewTutorial(@PathVariable String tutorial, Model model) {
+        model.addAttribute("guide", this.tutorials.findByName(tutorial).get());
+        model.addAttribute("description", "this tutorial is designed to be completed in 2-3 hours, it provides deeper," +
+                " in-context explorations of enterprise application development topics, leaving you ready to implement real-world solutions.");
+        return "guides/gs/guide";
+    }
+
+
+	@GetMapping("/{tutorial}/images/{image:[a-zA-Z0-9._-]+}")
+	public ResponseEntity<byte[]> loadImage(@PathVariable String tutorial, @PathVariable String image) {
+		return this.tutorials.findByName(tutorial)
+				.flatMap(gs -> gs.getImageContent(image))
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+}
